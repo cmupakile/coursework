a6a2c4168ef829d3f199043e00b05b06eef9d060
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-03-20 14:47:36 +0000

    mid day commit

--- w02d01/blog/config/routes.rb
+++ w02d01/blog/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  resources :posts #creates the RESTFUL routes
+
+  # get   'posts', to: 'posts#index'
+  #
+  # get  'posts/new', to: 'posts#new'
+  #
+  # put  'posts/:id', to: 'posts#update'
+  #
+  # delete  'posts/:id' , to:'posts#destroy'
+  #
+  # get 'posts/:id' , to: 'posts#show' #show one post
+  #
+  # get  'posts/:id/edit', to: 'posts#edit'
+  #
+  # post '/posts' , to: 'posts#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
