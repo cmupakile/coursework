c9e0f5bf5d2edaea8f42f0349ff022de5273c85f
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-03-22 16:27:31 +0000

    link table created

--- w02d02/instagram2/app/models/picture.rb
+++ w02d02/instagram2/app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ApplicationRecord
 has_many :comments
 belongs_to :user
+has_and_belongs_to_many :liked , class_name: “User”   
 end
--- w02d02/instagram2/db/schema.rb
+++ w02d02/instagram2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322100256) do
+ActiveRecord::Schema.define(version: 20170322160721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20170322100256) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_pictures_on_user_id", using: :btree
+  end
+
+  create_table "pictures_users", id: false, force: :cascade do |t|
+    t.integer "user_id",    null: false
+    t.integer "picture_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
