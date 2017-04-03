1c8e2c93f9f608d3ae81aad66cfad2fcc9104307
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-04-03 18:50:57 +0100

    environment setup.source list update.package upgrade. git package.

---
+++ w04d01/home_Lab/starter-code (1)/provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#update the sources list
+sudo apt-get update -y
+#upgrade any packages available
+sudo apt-get upgrade -y
+#install git
+sudo apt-get install git -y
+# install ruby
+
+# install bundler
+
+# install rake
+
+# install rails
+
+# install apache2
+# enable apache2 and run it
